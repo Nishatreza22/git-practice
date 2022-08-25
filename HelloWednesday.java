6efe57d5956b9446598c62b7e4ca5f9699974ce1
unknown <nishat22it@gmail.com>
2022-08-25 10:41:29 -0400

    new commit

---
+++ HelloWednesday.java
@@ -0,0 +1,17 @@
+package com.git.practice;
+
+public class HelloWednesday {
+    public static void main(String[] args) {
+
+        System.out.println(helloEveryone());
+
+    }
+
+    public static String helloEveryone(){
+        return "Hi! How r You Everyone.";
+    }
+
+
+
+
+}
